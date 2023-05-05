7e82ee2d5c507dface3dae4055f5d9960fbc7efa
ahfish <ahfish@gmail.com>
2023-05-05 22:48:02 +0100

    update

--- marketdata.sql
+++ marketdata.sql
@@ -1388,10 +1388,14 @@ from
  select DATE_TRUNC('day', time), count(1) from ctrader_market_price_zg_every_minute group by DATE_TRUNC('day', time) order by DATE_TRUNC('day', time) desc
  select DATE_TRUNC('day', time), count(1) from ctrader_market_price_gbpjpy_every_fifteen_minute cmpgefm group by DATE_TRUNC('day', time) order by DATE_TRUNC('day', time) desc
  select DATE_TRUNC('day', time), count(1) from market_price_zg_every_minute group by DATE_TRUNC('day', time) order by DATE_TRUNC('day', time) desc
+ select DATE_TRUNC('day', time), count(1) from market_price_zg_every_five_minute group by DATE_TRUNC('day', time) order by DATE_TRUNC('day', time) desc
  select DATE_TRUNC('day', time), count(1) from market_price_gbpjpy_every_minute group by DATE_TRUNC('day', time) order by DATE_TRUNC('day', time) desc
+  select DATE_TRUNC('day', time), count(1) from market_price_gbpusd_every_minute group by DATE_TRUNC('day', time) order by DATE_TRUNC('day', time) desc
+ select DATE_TRUNC('day', time), count(1) from market_price_gbpjpy_every_five_minute group by DATE_TRUNC('day', time) order by DATE_TRUNC('day', time) desc
   select DATE_TRUNC('day', time), count(1) from market_price_gbpjpy_every_fifteen_minute group by DATE_TRUNC('day', time) order by DATE_TRUNC('day', time) desc
  SELECT DATE_TRUNC('hour', TIMESTAMP '2017-03-17 02:09:30');
  
+select * from market_price_gbpjpy_every_minute order by time desc
 
 select * from market_price_gbpjpy_every_fifteen_minute where time >= '2022-11-25 18:45:00' and time <= '2022-11-28 08:30:00'  order by time 
 
