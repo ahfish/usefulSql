6def815e58b4c3ae9f91660e34fe8ce4ed5477ea
ahfish <ahfish@gmail.com>
2023-12-08 21:51:57 +0000

    update

--- marketdata.sql
+++ marketdata.sql
@@ -1396,7 +1396,8 @@ from
  select DATE_TRUNC('day', time), count(1) from market_price_gbpusd_every_five_minute group by DATE_TRUNC('day', time) order by DATE_TRUNC('day', time) desc
   select DATE_TRUNC('day', time), count(1) from market_price_gbpjpy_every_fifteen_minute group by DATE_TRUNC('day', time) order by DATE_TRUNC('day', time) desc
  SELECT DATE_TRUNC('hour', TIMESTAMP '2017-03-17 02:09:30');
- 
+
+select * from market_price_zg_every_five_minute
 select * from market_price_gbpjpy_every_minute order by time desc
 
 select *, high-low from market_price_gbpjpy_every_fifteen_minute where time >= '2022-10-11 19:15:00' and time <= '2022-10-11 20:15:00'  order by time 
